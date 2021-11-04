e0e566b177bb9b8c221024c1884c9c6d5107cdac
AlexApukhtin <aac94@yandex.ru>
2021-11-05 00:43:05 +0300

    task project6

---
+++ src/main/java/ru/gb/animal/Other.java
@@ -0,0 +1,36 @@
+package ru.gb.animal;
+
+public class Other extends Animal {
+
+    private static int otherScore;
+
+    public Other(String name, int run, int swim, String view) {
+        super(name, run, swim);
+        setView(view);
+        otherScore++;
+    }
+
+    public static int getOtherScore() {
+        return otherScore;
+    }
+
+    public void setView(String view) {
+        if (view.length() != 0) this.view = view;
+    }
+
+    @Override
+    public void setRun(int run) {
+        this.run = run;
+    }
+
+    @Override
+    public void setSwim(int swim) {
+        this.swim = swim;
+    }
+
+    @Override
+    public void print(int score, int length) {
+        System.out.printf("Это %s. %s пробежал %dм. и проплыл %dм.\n", getView(), getName(), getRun(), getSwim());
+    }
+}
+
